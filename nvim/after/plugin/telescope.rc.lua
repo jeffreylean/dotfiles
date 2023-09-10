a45138c0079dd8df346b10b5d93323f90b13a91a
JeffreyLean <leanwf1117@gmail.com>
2023-09-10 17:49:35 +0800

    Add telescope search document symbol bindings

--- nvim/after/plugin/telescope.rc.lua
+++ nvim/after/plugin/telescope.rc.lua
@@ -39,4 +39,6 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>ss', require('telescope.builtin').lsp_workspace_symbols,
-{ desc = '[S]earch workspace [S]mbol' })
+    { desc = '[S]earch workspace [S]mbol' })
+vim.keymap.set('n', '<leader>sds', require('telescope.builtin').lsp_document_symbols,
+{ desc = '[S]earch document [S]ymbol' })
