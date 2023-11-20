b982c1ddb3b36437c71e48034259ddd137973650
jeffreylean <leanwf1117@gmail.com>
2023-11-20 14:25:34 +0800

    Add linux config

---
+++ linux/lid_monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true; do
+    lid_state=$(cat /proc/acpi/button/lid/LID/state | awk '{print $2}')
+    if [ "$lid_state" = "closed" ]; then
+        xrandr --output eDP-1 --off # Replace LVDS1 with your laptop monitor identifier
+    else
+        xrandr --output eDP-1 --auto # Replace LVDS1 with your laptop monitor identifier
+    fi
+    sleep 5 # Check every 5 seconds
+done
